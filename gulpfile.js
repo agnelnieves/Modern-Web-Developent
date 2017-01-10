c450a631d6ebf9610bb25d7c39630d04cfd70ca8
Agnel Nieves <agnelnieves@Agnels-MacBook-Pro.local>
2017-01-10 15:42:29 -0400

    added tasks in gulp and scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 //Add all the tasks to run in gulp
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//Sass Compiling task
+gulp.task('sass', function(){
+  return gulp.src('src/scss/app.scss')
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('default', ['sass']);
